a38d9d7f51e0fe67d7d2793c1b8105c5574dbfd3
Alexandre Lairan <lairana@free.fr>
2020-05-15 12:26:00 +0200

    Make tests pass

--- spec/model/hiring/candidate_spec.cr
+++ spec/model/hiring/candidate_spec.cr
@@ -4,7 +4,7 @@ Spectator.describe Hiring::Candidate do
   context "setup from DTO" do
     subject { described_class.new(data) }
 
-    let(data) { DTO::Candidate.new(id: 0, name: "Arthur", skills: Array(DTO::Skill).new) }
+    let(data) { DTO::Candidate.new(id: 0, name: "Arthur", skills: Array(DTO::Skill).new, availability: DTO::Availability.new(Time.utc, Time.utc)) }
 
     it { is_expected.to be_a(Hiring::Candidate) }
   end
--- spec/model/hiring/recruiter_spec.cr
+++ spec/model/hiring/recruiter_spec.cr
@@ -4,7 +4,7 @@ Spectator.describe Hiring::Recruiter do
   context "setup from DTO" do
     subject { described_class.new(data) }
 
-    let(data) { DTO::Recruiter.new(id: 0, name: "Stan", skills: Array(DTO::Skill).new) }
+    let(data) { DTO::Recruiter.new(id: 0, name: "Stan", skills: Array(DTO::Skill).new, availabilities: [DTO::Availability.new(Time.utc, Time.utc)]) }
 
     it { is_expected.to be_a(Hiring::Recruiter) }
   end
