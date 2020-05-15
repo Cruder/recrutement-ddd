932bf7e15cd8eb73af6d778f859aac0424db7e8e
Alexandre Lairan <lairana@free.fr>
2020-05-15 16:37:13 +0200

    Split Availability with busy time

--- src/model/hiring/availability.cr
+++ src/model/hiring/availability.cr
@@ -3,19 +3,30 @@ module Hiring
     protected getter start_at : Time
     protected getter end_at : Time
 
+    def self.make_busy_time(actual, busy) : {Availability, Availability}
+      beginning = Availability.new(actual.start_at, busy.start_at)
+      ending = Availability.new(busy.end_at, actual.end_at)
+
+      {beginning, ending}
+    end
+
     def initialize(availability : DTO::Availability)
       initialize(availability.start_at, availability.end_at)
     end
 
     def initialize(@start_at : Time, @end_at : Time)
-      # Verifier un availability not in weekend
-      # Heure de debut < heure de fin
-      # C'est bien le soir
+      raise "Starts on weekend" if @start_at.saturday? || @start_at.sunday?
+      raise "Ends on weekend" if @end_at.saturday? || @end_at.sunday?
+      raise "end_at must be greater than start_at" if @end_at < @start_at
     end
 
     def match?(availability : Availability)
       start_at <= availability.start_at &&
         end_at >= availability.end_at
+    end
+
+    def ==(availability : Availability)
+      start_at == availability.start_at && end_at == availability.end_at
     end
   end
 end
